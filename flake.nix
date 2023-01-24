c5fa69230f53bf9fa99601b91f4a231b508c1b71
Vincent Fischer <fischervincent98@gmail.com>
2023-01-24 19:41:38 +0100

    nix

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Enterprise grade food ordering system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = { self, flake-utils, naersk, nixpkgs, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+          (import rust-overlay)
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+
+        };
+
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        dev-toolchain = toolchain.override {
+          extensions = [ "rust-src" "rls" "rustfmt" ];
+        };
+        
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+      in rec {
+
+        defaultPackage = naersk'.buildPackage {
+          src = ./.;
+        };
+        
+        devShell = pkgs.mkShell rec {
+          name = "napoli-rust";
+
+          packages = with pkgs; [
+            # Development Tools
+            dev-toolchain
+            pkgconf
+            pkg-config
+            protobuf
+          ];
+        };
+      }
+    );
+}
