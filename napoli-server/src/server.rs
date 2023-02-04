ff5033d7787a89f396eadc68012887bc6303f47f
Maximilian Berger <max@geile.software>
2023-02-04 01:26:55 +0100

    Implement RemoveOrderEntry, GetOrder, UpdateOrderState, SetOrderEntryPaid gRPC calls

--- napoli-server/src/server.rs
+++ napoli-server/src/server.rs
@@ -1,12 +1,12 @@
 use napoli_lib::napoli::order_service_server::OrderService;
 use napoli_lib::napoli::{
-    AddOrderEntryRequest, CreateOrderRequest, GetOrdersReply, GetOrdersRequest, OrderEntryRequest,
-    SetOrderEntryPaidRequest, SingleOrderReply,
+    AddOrderEntryRequest, CreateOrderRequest, GetOrderRequest, GetOrdersReply, GetOrdersRequest,
+    OrderEntryRequest, SetOrderEntryPaidRequest, SingleOrderReply, UpdateOrderStateRequest,
 };
 
 use napoli_server_persistent_entities::order;
 use napoli_server_persistent_entities::order_entry;
-use sea_orm::{ActiveModelTrait, ModelTrait};
+use sea_orm::{ActiveModelTrait, ModelTrait, Set};
 use sea_orm::{DatabaseConnection, EntityTrait};
 use sea_orm::{IntoActiveModel, QueryOrder as _};
 use tonic::{Request, Response, Status};
@@ -41,6 +41,30 @@ impl OrderService for NapoliServer {
             .collect();
 
         Ok(Response::new(GetOrdersReply { orders }))
+    }
+
+    async fn get_order(
+        &self,
+        request: Request<GetOrderRequest>,
+    ) -> Result<Response<SingleOrderReply>, Status> {
+        let order_id = request.into_inner().order_id;
+        let orders = order::Entity::find_by_id(order_id)
+            .find_with_related(order_entry::Entity)
+            .all(&self.db_handle)
+            .await
+            .map_err(map_to_status)?;
+
+        let (order, entries) = match orders.into_iter().next() {
+            Some((order, entries)) => (order, entries),
+            None => return Err(Status::not_found("order not found")),
+        };
+
+        Ok(Response::new(SingleOrderReply {
+            order: Some(model_adapters::database_order_to_tonic_order(
+                order,
+                entries.into_iter(),
+            )),
+        }))
     }
 
     async fn create_order(
@@ -118,18 +142,111 @@ impl OrderService for NapoliServer {
         }
     }
 
+    async fn update_order_state(
+        &self,
+        request: Request<UpdateOrderStateRequest>,
+    ) -> Result<Response<SingleOrderReply>, Status> {
+        let request = request.into_inner();
+        let order_id = request.order_id.to_owned();
+        let orders = order::Entity::find_by_id(order_id)
+            .one(&self.db_handle)
+            .await
+            .map_err(map_to_status)?;
+
+        let order = match orders {
+            Some(order) => order,
+            None => return Err(Status::not_found("order not found")),
+        };
+
+        let mut order = order.into_active_model();
+        order.state = Set(request.state.to_owned());
+
+        let order: order::Model = order.update(&self.db_handle).await.map_err(map_to_status)?;
+        let entries = order
+            .find_related(order_entry::Entity)
+            .all(&self.db_handle)
+            .await
+            .map_err(map_to_status)?;
+
+        Ok(Response::new(SingleOrderReply {
+            order: Some(model_adapters::database_order_to_tonic_order(
+                order,
+                entries.into_iter(),
+            )),
+        }))
+    }
+
     async fn remove_order_entry(
         &self,
         request: Request<OrderEntryRequest>,
     ) -> Result<Response<SingleOrderReply>, Status> {
-        todo!()
+        let request = request.into_inner();
+
+        let _order_entry = order_entry::Entity::delete_by_id(request.order_entry_id)
+            .exec(&self.db_handle)
+            .await
+            .map_err(map_to_status)?;
+
+        let orders = order::Entity::find_by_id(request.order_id)
+            .find_with_related(order_entry::Entity)
+            .all(&self.db_handle)
+            .await
+            .map_err(map_to_status)?;
+
+        let (order, entries) = match orders.into_iter().next() {
+            Some(order) => order,
+            None => return Err(Status::not_found("order not found")),
+        };
+
+        Ok(Response::new(SingleOrderReply {
+            order: Some(model_adapters::database_order_to_tonic_order(
+                order,
+                entries.into_iter(),
+            )),
+        }))
     }
 
     async fn set_order_entry_paid(
         &self,
         request: Request<SetOrderEntryPaidRequest>,
     ) -> Result<Response<SingleOrderReply>, Status> {
-        todo!()
+        let request = request.into_inner();
+
+        let order_entry = order_entry::Entity::find_by_id(request.order_entry_id)
+            .one(&self.db_handle)
+            .await
+            .map_err(map_to_status)?;
+
+        let order_entry = match order_entry {
+            Some(order_entry) => order_entry,
+            None => return Err(Status::not_found("order entry not found")),
+        };
+
+        let mut order_entry = order_entry.into_active_model();
+        order_entry.paid = Set(request.paid);
+
+        let _order_entry: order_entry::Model = order_entry
+            .update(&self.db_handle)
+            .await
+            .map_err(map_to_status)?;
+
+        let orders = order::Entity::find_by_id(request.order_id)
+            .find_with_related(order_entry::Entity)
+            .all(&self.db_handle)
+            .await
+            .map_err(map_to_status)?;
+
+        let (order, entries) = match orders.into_iter().next() {
+            Some(order) => order,
+            None => return Err(Status::not_found("order not found")),
+        };
+
+        Ok(Response::new(SingleOrderReply {
+            order: Some(model_adapters::database_order_to_tonic_order(
+                order,
+                entries.into_iter(),
+            )),
+        }))
     }
 }
 
